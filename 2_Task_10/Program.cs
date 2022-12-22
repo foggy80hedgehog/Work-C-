bb2ba89e7ce3eea959d1dce15ffcf302a0a111f3
foggy_hedgehog <foggy80hedgehog@gmail.com>
2022-12-23 00:36:25 +0300

    2_Task_10

---
+++ 2_Task_10/Program.cs
@@ -0,0 +1,21 @@
+﻿//  Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа. 
+ 
+//456 -> 5 
+//782 -> 8 
+//918 -> 1 
+ 
+ 
+Console.Clear(); 
+Console.WriteLine("вход трёхзначное число и на выходе показывает вторую цифру этого числа"); 
+Console.WriteLine(); 
+Console.WriteLine("Введите трехзначное число: "); 
+int number =int.Parse (Console.ReadLine() ); 
+ 
+if (number > 99 && number < 1000) 
+   {  
+    number = number / 10 % 10; 
+    Console.WriteLine($"Вторая цифра вашего числа : {number}"); 
+   } 
+ 
+   else 
+    Console.WriteLine("Неверное значение числа");
