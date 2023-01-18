28d4c28e42d2703e13c1f3e3fe65019e600e19bb
foggy_hedgehog <foggy80hedgehog@gmail.com>
2023-01-19 00:56:54 +0300

    4_Task_29

---
+++ 4_Task_29/Program.cs
@@ -0,0 +1,19 @@
+﻿/* 
+Напишите программу, которая задаёт массив и выводит его на экран.
+
+1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
+
+6, 1, 33 -> [6, 1, 33]
+
+все  элементы должны получить за один Console.ReadLine();
+подсказка: использовать потом метод string.Split();
+
+*/
+
+Console.Clear();
+Console.WriteLine ("Введите значения в массив через пробел");
+string[] Mass=Console.ReadLine().Split(' ');
+Console.WriteLine();
+Console.WriteLine("Ваш массив: {0}",String.Join(",",Mass));             
+
+
